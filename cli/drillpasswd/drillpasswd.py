1301a24beffe43608aa96cac8dc9f0857f0d7569
Luciano Ramalho <luciano@ramalho.org>
2017-12-26 12:11:41 -0200

    drillpassword in Python 3

---
+++ cli/drillpasswd/drillpasswd.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+"""drillpaswd: finger drills for practicing passphrases"""
+
+import sys
+import hashlib
+import base64
+import getpass
+
+HASH_ALGORITHM = 'sha512'
+PASSPHRASE_HASH_FILENAME = 'drillpasswd.' + HASH_ALGORITHM
+
+
+def prompt():
+    print('WARNING: the passphrase will be shown so that you can check it!')
+    confirmed = ''
+    while confirmed != 'y':
+        passwd = input('Type passphrase to hash (it will be echoed): ')
+        if passwd == '' or passwd == 'q':
+            print('ERROR: the passphrase cannot be empty or "q".')
+            continue
+        print(f'Passphrase to be hashed -> {passwd}')
+        confirmed = input('Confirm (y/n): ').lower()
+    return passwd
+
+
+def hasher(text):
+    h = hashlib.new(HASH_ALGORITHM, text.encode('utf-8'))
+    return base64.encodebytes(h.digest())
+
+
+def save_hash(argv):
+    if len(argv) > 1 or argv[0] != '-s':
+        print('ERROR: invalid argument. Use -s to save passphrase hash for practice.')
+        sys.exit(1)
+    passwd_hash = hasher(prompt())
+    with open(PASSPHRASE_HASH_FILENAME, 'wb') as fp:
+        fp.write(passwd_hash)
+    print(f'Passphrase {HASH_ALGORITHM} hash saved to {PASSPHRASE_HASH_FILENAME}.')
+
+
+def practice():
+    with open(PASSPHRASE_HASH_FILENAME, 'rb') as fp:
+        passwd_hash = fp.read()
+    print('Type q to end practice.')
+    response = ''
+    turn = 0
+    correct = 0
+    while True:
+        turn += 1
+        response = getpass.getpass(f'{turn}:')
+        if response == '':
+            print('Type q to quit.')
+            turn -= 1  # don't count this turn
+            continue
+        elif response == 'q':
+            turn -= 1  # don't count this turn
+            break
+        if hasher(response) == passwd_hash:
+            correct += 1
+            answer = 'OK'
+        else:
+            answer = 'wrong'
+        print(f'  {answer}\thits={correct}\tmisses={turn-correct}')
+
+    if turn:
+        pct = correct / (turn) * 100
+        print(f'\n{turn} exercises. {pct:0.1f}% correct.')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        save_hash(sys.argv[1:])
+    else:
+        practice()
